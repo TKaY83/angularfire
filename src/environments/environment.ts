a25e06d5072d659fadb696438adad45aff4b7745
Tuncay Dagdelen <sramos083@gmail.com>
2022-09-06 10:54:40 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'tdangulartodo',
+    appId: '1:522743637284:web:438e9a51ad2f11362bffb1',
+    databaseURL: 'https://tdangulartodo-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'tdangulartodo.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyBom0nj1N6btH0KkvH6Bhw4khie1dq-OdM',
+    authDomain: 'tdangulartodo.firebaseapp.com',
+    messagingSenderId: '522743637284',
+    measurementId: 'G-PDPNZDB2EB',
+  },
   production: false
 };
 
